86159598b6c8486c373e17e2c7351ebd83612c07
Ruchika sharma <ruchikas@geekyants.com>
2022-07-05 12:22:20 +0530

    set cookie in php

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document of Get Method</title>
-</head>
+<?php
+$cookie_name = "user";
+$cookie_value = "Ruchika Sharma";
+setcookie($cookie_name, $cookie_value, time()+4, "/"); // 86400 = 1 day
+?>
+<html>
 <body>
-    <form action="submit.php" method="post">
-        <label for="inputNumber">Enter a number:</label>
-        <input type="number" name="num" id="inputNumber">
-         <input type="submit" value="submit">
-    </form>
+
+<?php
+if(isset($_COOKIE[$cookie_name])) {
+    echo "Cookie " . $cookie_name . " is set!<br>";
+    echo "Value is: " . $_COOKIE[$cookie_name];
+  
+} else {
+  echo "Cookie named " . $cookie_name . " is not set!";
+}
+?>
+
 </body>
-</html>     
+</html>
